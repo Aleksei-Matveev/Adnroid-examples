911bacbc32d401940f16ae6411e98d03536c46d2
Matveev Aleksey <matveevalex1512@gmail.com>
2019-03-01 19:05:49 +0300

    Text View example

---
+++ textview/src/main/java/by/matveev/textview/TextView.java
@@ -0,0 +1,13 @@
+package by.matveev.textview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TextView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_text_view);
+    }
+}
