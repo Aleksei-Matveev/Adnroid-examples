d27909fe9a6182d1636d55ede04d772e22bd6993
Matveev Aleksey <matveevalex1512@gmail.com>
2019-03-01 19:15:03 +0300

    Text View example

--- app/src/main/java/by/matveev/androidexamples/MainActivity.java
+++ app/src/main/java/by/matveev/androidexamples/MainActivity.java
@@ -1,17 +1,13 @@
-package by.matveev.textview;
+package by.matveev.androidexamples;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class TextView extends AppCompatActivity {
-
-    TextView txt1, txt2, txt3, txt4;
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_text_view);
-
-
+        setContentView(R.layout.activity_main);
     }
 }
