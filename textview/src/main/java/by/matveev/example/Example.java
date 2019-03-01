030f345ea4f9fbe009664e9ef2769ef519b43d6a
Aleksey Matveev <matveevalex1512@gmail.com>
2019-03-01 21:12:33 +0300

    add Text ViewExamples

--- textview/src/main/java/by/matveev/example/Example.java
+++ textview/src/main/java/by/matveev/example/Example.java
@@ -2,7 +2,9 @@ package by.matveev.example;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class Example extends AppCompatActivity {
@@ -24,6 +26,47 @@ public class Example extends AppCompatActivity {
         txt2.setTextIsSelectable(true);
         txt3.setTextIsSelectable(true);
         txt4.setTextIsSelectable(true);
+
+        /***Set text in TextView Programmatically***/
+        txt1.setText("This is text set in TextView Programmatically");
+
+        /***Set the Visibility of TextView
+         * [View.GONE = Not Visible]
+         * [View.VISIBLE = Visible***/
+
+        txt1.setVisibility(View.GONE);
+        txt1.setVisibility(View.VISIBLE);
+
+        /***Set Background color of TextView Programmatically***/
+        txt2.setBackgroundColor(getResources().getColor(android.R.color.holo_red_dark));
+
+        /***Onclick Listener will fire when click on any TextView***/
+        txt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txt1.setVisibility(View.GONE);
+                Toast.makeText(Example.this, "TextView1", Toast.LENGTH_SHORT).show();
+            }
+        });
+        txt2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(Example.this, "TextView2", Toast.LENGTH_SHORT).show();
+            }
+        });
+        txt3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(Example.this, "TextView3", Toast.LENGTH_SHORT).show();
+            }
+        });
+        txt4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(Example.this, "TextView4", Toast.LENGTH_SHORT).show();
+            }
+        });
+
 
     }
 }
